38c7342f4d77cec8bdecded6f5ca16cda1e0ed29
Yunkon (Alvin) Kim <hermitkim1@gmail.com>
2020-11-03 11:53:39 +0900

    Add the contributors metric and organization metrics
    - Add the number of contributors for each repository
    - Add the number of repository and member for organization
    - Update gitignore not to upload *.csv

--- main.py
+++ main.py
@@ -16,42 +16,73 @@ if __name__ == '__main__':
 
     auth_file.close()
 
+    today = date.today()
+    this_year = today.year
+
+    # Authentication
+    gh_session = requests.Session()
+    gh_session.auth = (username, password)
+
     # Rate limiting: https://developer.github.com/v3/#rate-limiting
     api_root = "http://api.github.com/repos/"
 
-    # GitHub API example
+    # Test code - GitHub API example
     #  http://api.github.com/repos/[username]/[reponame]
     # link = "http://api.github.com/repos/cloud-barista/cb-spider"
-    # with urllib.request.urlopen(link) as url:
-    #     data = json.loads(url.read().decode())
-    #     pretty_json = json.dumps(data, indent=4, sort_keys=True)
-    #     print(pretty_json)
+    # data = json.loads(gh_session.get(link).text)
+    # pretty_json = json.dumps(data, indent=4, sort_keys=True)
+    # print(pretty_json)
     #
     # print("Stars: %s" % (data["stargazers_count"]))
     # print("Forks: %s" % (data["forks_count"]))
     # print("Watches: %s" % (data["subscribers_count"]))
 
-    result_file = open("Result.csv", "w", newline="")
-    csv_writer = csv.writer(result_file)
+    #############################################################################
+    # Result of organization
+    orgs_result_file = open("orgs-result.csv", "w", newline="")
+    orgs_result_writer = csv.writer(orgs_result_file)
 
+    # Header
+    orgs_result_writer.writerow(["Organization", "Repositories", "Members"])
+
+    org_link = "https://api.github.com/orgs/cloud-barista"
+    org_info = json.loads(gh_session.get(org_link).text)
+
+    members_link = "https://api.github.com/orgs/cloud-barista/members"
+    members_info = json.loads(gh_session.get(members_link).text)
+    print("Organization name: %s" % org_info["name"])
+    print("Public repos: %s" % (org_info["public_repos"]))
+    print("Members: %s" % (len(members_info)))
+
+    orgs_result_writer.writerow([org_info["name"], org_info["public_repos"], len(members_info)])
+
+    orgs_result_file.close()
+
+    #############################################################################
+    # Results of each repository
+    repos_result_file = open("repos-result.csv", "w", newline="")
+    repos_result_writer = csv.writer(repos_result_file)
+
+    # Header
+    repos_result_writer.writerow(
+        ["Repo", "Contributors", "Stars", "Forks", "Watches", "Commits(" + str(this_year) + ")"])
+
+    # Read repos
     with open('repos.json') as json_file:
         repos = json.load(json_file)
 
-    today = date.today()
-    this_year = today.year
-
-    csv_writer.writerow(["Repo", "Stars", "Forks", "Watches", "Commits(" + str(this_year) + ")"])
     for repo in repos:
         print("\n%s(%s)" % (repo["Name"], repo["Path"]))
         repo_link = api_root + repo["Path"]
-
-        gh_session = requests.Session()
-        gh_session.auth = (username, password)
 
         # Request repository statistics/information to get the number of stars, forks, watches
         repo_info = json.loads(gh_session.get(repo_link).text)
         # pretty_json = json.dumps(repo_info, indent=4, sort_keys=True)
         # print(pretty_json)
+
+        contributors_link = repo_info["contributors_url"]
+        contributors = json.loads(gh_session.get(contributors_link).text)
+        number_of_contributors = len(contributors)
 
         # Request the number of monthly commits
         # reference: https://docs.github.com/en/free-pro-team@latest/rest/reference/repos#list-commits
@@ -85,15 +116,17 @@ if __name__ == '__main__':
         commits2 = json.loads(gh_session.get(commit_link2).text)
         number_of_commits.append(int(len(commits2)))
 
+        print("Contributors: %s" % number_of_contributors)
         print("Stars: %s" % (repo_info["stargazers_count"]))
         print("Forks: %s" % (repo_info["forks_count"]))
         print("Watches: %s" % (repo_info["subscribers_count"]))
         print("Commits in this year: %s" % (sum(number_of_commits)))
 
-        csv_writer.writerow(
-            [repo["Name"], repo_info["stargazers_count"], repo_info["forks_count"], repo_info["subscribers_count"],
+        repos_result_writer.writerow(
+            [repo["Name"], number_of_contributors, repo_info["stargazers_count"], repo_info["forks_count"],
+             repo_info["subscribers_count"],
              sum(number_of_commits)])
 
     json_file.close()
-    result_file.close()
+    repos_result_file.close()
     gh_session.close()
